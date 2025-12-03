40e036fb47cbba7e26f204fb78c56228a24b147b
CaptainPickles89 <d.nicholls89@yahoo.co.uk>
2025-12-03 10:31:14 +0000

    Closing image in birthday.py

--- birthdays.py
+++ birthdays.py
@@ -40,35 +40,35 @@ def check_birthdays():
     if birthday_matches:
         print("There are birthdays today!")
         inky = auto()
-        img = Image.open("./resources/imgs/birthday-bg1-01.png")
-        draw = ImageDraw.Draw(img)
+        with Image.open("./resources/imgs/birthday-bg1-01.png") as img:
+            draw = ImageDraw.Draw(img)
 
-        # Font settings (update path to your font file)
-        font_path = "./resources/fonts/Roboto-Medium.ttf"
-        font = ImageFont.truetype(font_path, 50)
+            # Font settings (update path to your font file)
+            font_path = "./resources/fonts/Roboto-Medium.ttf"
+            font = ImageFont.truetype(font_path, 50)
 
-        # Birthday message for the inky
-        names = "\n".join(birthday_matches)
-        message = f"Birthdays Today!\n{names}"
+            # Birthday message for the inky
+            names = "\n".join(birthday_matches)
+            message = f"Birthdays Today!\n{names}"
 
-        # Measure text size
-        text_bbox = draw.multiline_textbbox((0, 0), message, font=font)
-        text_width = text_bbox[2] - text_bbox[0]
-        text_height = text_bbox[3] - text_bbox[1]
+            # Measure text size
+            text_bbox = draw.multiline_textbbox((0, 0), message, font=font)
+            text_width = text_bbox[2] - text_bbox[0]
+            text_height = text_bbox[3] - text_bbox[1]
 
-        # Calculate position to center the text
-        image_width, image_height = img.size
-        x_position = (image_width - text_width) // 2
-        y_position = (image_height - text_height) // 2
+            # Calculate position to center the text
+            image_width, image_height = img.size
+            x_position = (image_width - text_width) // 2
+            y_position = (image_height - text_height) // 2
 
-        # Draw the text on the image
-        draw.multiline_text(
-            (x_position, y_position), message, font=font, align="center", fill=0
-        )  # Black text
-        # Show on the Inky
-        inky.set_image(img)
-        inky.show()
-        return birthday_matches  # Return the list of names for further use if needed
+            # Draw the text on the image
+            draw.multiline_text(
+                (x_position, y_position), message, font=font, align="center", fill=0
+            )  # Black text
+            # Show on the Inky
+            inky.set_image(img)
+            inky.show()
+            return birthday_matches  # Return the list of names for further use if needed
     else:
         print("No birthdays today.")
         return None
