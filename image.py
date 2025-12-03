e46f02f6ddfc25d0a94a98cff6cfb26a4968c13a
CaptainPickles89 <d.nicholls89@yahoo.co.uk>
2025-12-03 10:28:53 +0000

    Closing image in image.py

--- image.py
+++ image.py
@@ -10,15 +10,15 @@ from inky.auto import auto
 
 def display_image(image_path):
     inky = auto()
-    image = Image.open(image_path)
     saturation = 0.5
-    resizedimage = image.resize(inky.resolution)
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", message="Busy Wait: Held high")
-        try:
-            inky.set_image(resizedimage, saturation=saturation)
-        except TypeError:
-            inky.set_image(resizedimage)
+    with Image.open(image_path) as image:
+        resizedimage = image.resize(inky.resolution)
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", message="Busy Wait: Held high")
+            try:
+                inky.set_image(resizedimage, saturation=saturation)
+            except TypeError:
+                inky.set_image(resizedimage)
 
     inky.show()
 
